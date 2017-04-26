71fd2c45ae3ce91981829f5bb48e5ff13b4fea2e
bvrlbxr <alexeymleontiev@gmail.com>
2017-04-26 22:14:41 +0300

    Add files via upload
    My first exercise

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int upper,lower,step;
+float fahr, celsius;
+lower=0;
+upper=290;
+step=1;
+fahr=0;
+printf("    Table with celsius and fahr matching\t\n");
+
+celsius=0;
+while (celsius<=upper)
+    {
+
+    fahr=(celsius*9)/5+32;
+    printf("%3.0f\t %6.1f\n",celsius,fahr);
+    celsius=step+celsius;
+    }
+return 0;
+}
+
